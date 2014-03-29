d4b6914b0ef287a0c04310ab542744fa951345ef
Matthias Le Brun <mlbli@me.com>
2014-03-29 18:38:34 +0100

    MASS UPDATES 💥✊

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp')
   , uglify = require('gulp-uglify')
   , jade = require('gulp-jade')
   , minify = require('gulp-minify-css')
+  , plumber = require('gulp-plumber')
+  , stylus = require('gulp-stylus')
+  , autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('scripts', function(){
   return gulp.src('scripts/index.js')
@@ -12,7 +15,14 @@ gulp.task('scripts', function(){
 })
 
 gulp.task('styles', function(){
-  return gulp.src('styles/**.css')
+  return gulp.src('styles/**')
+    .pipe(plumber())
+    .pipe(stylus({
+      set : [
+        'include css'
+      ]
+    }))
+    .pipe(autoprefixer())
     .pipe(minify())
     .pipe(gulp.dest('dist/styles'))
 })
@@ -23,16 +33,17 @@ gulp.task('images', function(){
 })
 
 gulp.task('pages', function(){
-  return gulp.src('pages/*.jade')
+  return gulp.src('pages/**.jade')
+    .pipe(plumber())
     .pipe(jade())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/'))
 })
 
 gulp.task('watch', function(){
   gulp.watch('images/**', ['images'])
   gulp.watch('scripts/**', ['scripts'])
   gulp.watch('styles/**', ['styles'])
-  gulp.watch('pages/**', ['pages'])
+  gulp.watch(['pages/**', 'partials/**', 'layouts/**'], ['pages'])
 })
 
 gulp.task('default', ['scripts', 'styles', 'images', 'pages'])
